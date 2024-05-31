ef6906738e0e5a6e7f368a7d96f9d7e31e8e62f6
Feby <mfebyrizki@gmail.com>
2024-05-31 18:34:34 +0700

    tugas

--- pertemuan7/resources/views/task/index.blade.php
+++ pertemuan7/resources/views/task/index.blade.php
@@ -1,15 +1,5 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Daftar Tugas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
+@extends('template')
+@section('content')
     <div class="container">
         <h1 class="text-center">Daftar Tugas</h1>
         <hr>
@@ -33,9 +23,4 @@
             @endforeach
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
